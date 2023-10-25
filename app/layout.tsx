8698dcfcf31ac6fcd125e1ed0d6f3212cfa4056a
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-10-25 12:13:09 +0530

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Open_Sans } from 'next/font/google'
 import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
 import { ThemeProvider } from '@/components/providers/theme-provider'
+import { ModalProvider } from '@/components/providers/modal-provider';
 import { cn } from '@/lib/utils'
 
 const inter = Open_Sans({ subsets: ['latin'] })
@@ -20,13 +21,14 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body className={cn(inter.className, "bg-white dark:bg-zinc-900")}>
+        <body className={cn(inter.className, "bg-white dark:bg-zinc-800")}>
           <ThemeProvider
             attribute='class'
             defaultTheme='dark'
             enableSystem={false}
             storageKey='discord-theme'
           >
+            <ModalProvider />
             {children}
           </ThemeProvider>
           </body>
