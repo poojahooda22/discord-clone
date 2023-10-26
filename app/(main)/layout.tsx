d95b16d4fd8ca959b2020a3c2c1ad422d1635d48
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-10-26 10:16:33 +0530

    updated layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -5,7 +5,7 @@ const MainLayout = ({ children }: {
 }) => {
     return (
         <div className="h-full">
-            <div className="hidden md:flex h-full w-[82px] z-30 flex-col fixed inset-y-0">
+            <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
                 <NavigationSidebar />
             </div>
             <main className="md:pl-[72px] h-full">
