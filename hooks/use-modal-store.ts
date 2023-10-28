a3194d7ca28e5eeffa249a889aa8c77c46ab4b19
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-10-28 13:44:28 +0530

    update use modal

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,10 +1,14 @@
  import { create } from "zustand";
-import { Server } from "@prisma/client";
+import { ChannelType, Server, Channel} from "@prisma/client";
 
- export type ModalType = "createServer" | "invite" | "editServer";
+ export type ModalType = "createServer" | "invite"
+  | "editServer" | "members" | "createChannel" | 
+  "leaveServer" | "deleteServer" | "deleteChannel" | "editChannel"; 
 
  interface ModalData {
-   server?: Server
+   server?: Server;
+   channel?: Channel;
+   channelType?: ChannelType;
  }
 
  interface ModalStore {
