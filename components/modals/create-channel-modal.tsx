ef8a166ac52e9abdcee6e08a27a3258ab2ed86f7
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-10-28 13:42:33 +0530

    create channel

--- components/modals/create-channel-modal.tsx
+++ components/modals/create-channel-modal.tsx
@@ -53,23 +53,23 @@ export const CreateChannelModal = () => {
   const params = useParams();
 
   const isModalOpen = isOpen && type === "createChannel";
-//   const { channelType } = data;
+  const { channelType } = data;
  
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: "",
-      type:  ChannelType.TEXT,
+      type: channelType || ChannelType.TEXT,
     }
   });
 
-//   useEffect(() => {
-//     if (channelType) {
-//       form.setValue("type", channelType);
-//     } else {
-//       form.setValue("type", ChannelType.TEXT);
-//     }
-//   }, [channelType, form]);
+  useEffect(() => {
+    if (channelType) { 
+      form.setValue("type", channelType);
+    } else {
+      form.setValue("type", ChannelType.TEXT);
+    }
+  }, [channelType, form]);
 
   const isLoading = form.formState.isSubmitting;
 
