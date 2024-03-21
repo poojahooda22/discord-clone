0c3ef184443c159f58cb447d472c95e1a9c6bd4c
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-21 21:04:20 +0530

    updated servers

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -74,7 +74,8 @@ export const ServerChannel = ({
             <Trash
               onClick={(e) => onAction(e, "deleteChannel")}
               className="hidden group-hover:block w-4 h-4 
-              text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
+              text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 
+              dark:hover:text-zinc-300 transition"
             />
           </ActionTooltip>
         </div>
