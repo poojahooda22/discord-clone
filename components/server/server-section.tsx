16d7f9581b1cc503e1bfb52016c5b39e3a4ce162
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-21 21:00:42 +0530

    updated server

--- components/server/server-section.tsx
+++ components/server/server-section.tsx
@@ -60,7 +60,8 @@ export const ServerSection = ({
             onClick={() => onOpen("members", { server })}
             className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
           >
-            <Settings className="h-4 w-4" />
+            <Settings 
+            className="h-4 w-4" />
           </button>
         </ActionTooltip>
       )}
