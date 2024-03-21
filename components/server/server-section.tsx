034aa4fc455e8cbe57ebf058c3d380172aec3acf
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-21 21:00:57 +0530

    updated server

--- components/server/server-section.tsx
+++ components/server/server-section.tsx
@@ -61,7 +61,7 @@ export const ServerSection = ({
             className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
           >
             <Settings 
-            className="h-4 w-4" />
+              className="h-4 w-4" />
           </button>
         </ActionTooltip>
       )}
