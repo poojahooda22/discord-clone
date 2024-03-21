2e73a61940376d5ace76762e57e23985274275ad
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-21 20:55:52 +0530

    updated avatar

--- components/user-avatar.tsx
+++ components/user-avatar.tsx
@@ -14,7 +14,8 @@ export const UserAvatar = ({
     return (
         <Avatar className={cn("h-7 w-7 md:h-10 md:w-10", className)}>
             <AvatarImage 
-                src={src} />
+                src={src} 
+                />
         </Avatar>
     )
 }
