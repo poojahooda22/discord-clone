5cac914bfa099f03dfee0e2feeb5003bd27786ef
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-21 21:22:46 +0530

    updated action

--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -17,7 +17,8 @@ export  const NavigationAction = () =>   {
                 <button className="group flex items-center"
                     onClick={() => onOpen("createServer")}
                 >
-                    <div className="flex mx-3 h-[48px] w-[48px] rounded-[24px] 
+                    <div 
+                    className="flex mx-3 h-[48px] w-[48px] rounded-[24px] 
                         group-hover:rounded-[16px] transition-all overflow-hidden 
                         items-center justify-center bg-background 
                         dark:bg-neutral-700 group-hover:bg-emerald-500">
