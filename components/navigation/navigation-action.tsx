c8100ad6541bca6d4c21997dce18cb578bd295b0
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-21 21:23:49 +0530

    updated

--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -21,7 +21,8 @@ export  const NavigationAction = () =>   {
                         className="flex mx-3 h-[48px] w-[48px] rounded-[24px] 
                         group-hover:rounded-[16px] transition-all overflow-hidden 
                         items-center justify-center bg-background 
-                        dark:bg-neutral-700 group-hover:bg-emerald-500">
+                        dark:bg-neutral-700 group-hover:bg-emerald-500"
+                        >
                         <Plus 
                             className="group-hover:text-white transition 
                             text-emerald-500"
