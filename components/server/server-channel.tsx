2a787092e45927d6f5d1abbe92010415d922c73a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-21 21:03:24 +0530

    add

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -66,7 +66,8 @@ export const ServerChannel = ({
             <Edit
               onClick={(e) => onAction(e, "editChannel")}
               className="hidden group-hover:block w-4 h-4 text-zinc-500 
-              hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
+              hover:text-zinc-600 dark:text-zinc-400 
+              dark:hover:text-zinc-300 transition"
             />
           </ActionTooltip>
           <ActionTooltip label="Delete">
