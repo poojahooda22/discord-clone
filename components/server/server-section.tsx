1b184327dff2054e5863362a608d703da42d6d28
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-21 21:00:03 +0530

    updated style

--- components/server/server-section.tsx
+++ components/server/server-section.tsx
@@ -53,7 +53,8 @@ export const ServerSection = ({
         </ActionTooltip>
       )}
       {role === MemberRole.ADMIN && sectionType === "members" && (
-        <ActionTooltip label="Manage Members" side="top">
+        <ActionTooltip label="Manage Members" side="top"
+        >
           <button
             onClick={() => onOpen("members", { server })}
             className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
